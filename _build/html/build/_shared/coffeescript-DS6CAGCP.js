869d9502fc0e4094a56cd5728e05288f89ce4b97
Miguel PVMR <miguelpvmr@gmail.com>
2026-03-16 01:37:37 -0500

    Update EDA notebook and myst configuration

--- _build/html/build/_shared/coffeescript-DS6CAGCP.js
+++ _build/html/build/_shared/coffeescript-DS6CAGCP.js
@@ -1,1 +1,1 @@
-import{a}from"/myst_assets_folder/_shared/chunk-FXUHF6NI.js";import"/myst_assets_folder/_shared/chunk-OZE3FFNP.js";export default a();
+import{a}from"/Machine-Learning-EEG/build/_shared/chunk-FXUHF6NI.js";import"/Machine-Learning-EEG/build/_shared/chunk-OZE3FFNP.js";export default a();
--- _build/html/build/_shared/cshtml-Z2PZNC24.js
+++ _build/html/build/_shared/cshtml-Z2PZNC24.js
@@ -1,1 +1,1 @@
-import{a}from"/myst_assets_folder/_shared/chunk-U6TCSJGZ.js";import"/myst_assets_folder/_shared/chunk-37MEAX4M.js";import"/myst_assets_folder/_shared/chunk-OZE3FFNP.js";export default a();
+import{a}from"/Machine-Learning-EEG/build/_shared/chunk-U6TCSJGZ.js";import"/Machine-Learning-EEG/build/_shared/chunk-37MEAX4M.js";import"/Machine-Learning-EEG/build/_shared/chunk-OZE3FFNP.js";export default a();
